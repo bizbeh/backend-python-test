96dd8b0f449ba11e62097e12d7ad8f5718734bf7
bizbeh <biz.beh@gmail.com>
2016-07-28 02:59:50 -0400

    Changes for all tasks except ORM
    I have done all tasks except ORM which is the last task

--- alayatodo/views.py
+++ alayatodo/views.py
@@ -1,12 +1,17 @@
 from alayatodo import app
+import json
+import math
+
 from flask import (
     g,
     redirect,
     render_template,
     request,
-    session
+    session,
+    flash
     )
 
+ItemPerPage = 5
 
 @app.route('/')
 def home():
@@ -45,19 +50,24 @@ def logout():
 
 @app.route('/todo/<id>', methods=['GET'])
 def todo(id):
-    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s'" % id)
+    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s' and user_id = '%s'" % (id , session['user']["id"]))
     todo = cur.fetchone()
     return render_template('todo.html', todo=todo)
 
+@app.route('/todo/<id>/json', methods=['GET'])
+def todo_json(id):
+    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s' and user_id = '%s'" % (id , session['user']["id"]))
+    todo = json.dumps(dict(cur.fetchone()))
+    return render_template('todo-json.html', todo=todo)
 
 @app.route('/todo', methods=['GET'])
 @app.route('/todo/', methods=['GET'])
 def todos():
     if not session.get('logged_in'):
         return redirect('/login')
-    cur = g.db.execute("SELECT * FROM todos")
+    cur = Get_todo(0)
     todos = cur.fetchall()
-    return render_template('todos.html', todos=todos)
+    return render_template('todos.html', todos=todos, cur=1)
 
 
 @app.route('/todo', methods=['POST'])
@@ -65,11 +75,16 @@ def todos():
 def todos_POST():
     if not session.get('logged_in'):
         return redirect('/login')
+    t_Desc = request.form.get('description', '').strip()
+    if t_Desc == '' :
+        flash('Please Enter a valid description')
+        return redirect('/todo')
     g.db.execute(
         "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
-        % (session['user']['id'], request.form.get('description', ''))
+        % (session['user']['id'], t_Desc)
     )
     g.db.commit()
+    flash('Your to do list was successfully added')
     return redirect('/todo')
 
 
@@ -79,4 +94,46 @@ def todo_delete(id):
         return redirect('/login')
     g.db.execute("DELETE FROM todos WHERE id ='%s'" % id)
     g.db.commit()
+    flash('Your to do list was successfully deleted')
     return redirect('/todo')
+
+@app.route('/todoUpdate/<id>', methods=['POST'])
+def todo_complete(id):
+    if not session.get('logged_in'):
+        return redirect('/login')
+    g.db.execute("UPDATE todos SET complete = 1 WHERE id ='%s'" % id)
+    g.db.commit()
+    flash('Congratulations! you have completed a to do list')
+    return redirect('/todo')
+
+@app.route('/todoNothing/', methods = ['POST'])
+def todo_Nothing():
+    pass
+    return redirect('/todo')
+
+@app.route('/todoPre/<idx>', methods = ['post'])
+def todo_Pre(idx):
+    if (int(idx) == 1):
+        temp_Idx = int(idx)
+    else:
+        temp_Idx = int(idx)-1
+    cur = Get_todo(((temp_Idx - 1) * ItemPerPage))
+    todos = cur.fetchall()
+    return render_template('todos.html', todos=todos, cur=temp_Idx)
+
+@app.route('/todoNext/<idx>', methods = ['post'])
+def todo_Next(idx):
+    cur = g.db.execute("SELECT COUNT(1) FROM todos WHERE user_id = '%s'" % session['user']["id"])
+    count = cur.fetchone()[0]
+    if (math.ceil(float(count)/ItemPerPage) < int(idx)+1):
+        temp_Idx = int(idx)
+    else:
+        temp_Idx = int(idx)+1
+    cur = Get_todo(((temp_Idx-1)*ItemPerPage))
+    todos = cur.fetchall()
+    return render_template('todos.html', todos=todos, cur=temp_Idx)
+
+def Get_todo(offset):
+    cur = g.db.execute("SELECT * FROM todos WHERE user_id = '%s' ORDER BY id ASC LIMIT '%s' OFFSET '%s'" % (
+    session['user']["id"], str(ItemPerPage), str(offset)))
+    return cur
